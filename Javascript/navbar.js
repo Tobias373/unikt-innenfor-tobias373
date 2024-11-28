7c63f4498222646f94f652c83e78977b7fe49582
John Doe <johndoe@email.com>
2024-11-28 08:28:28 +0100

    e

---
+++ Javascript/navbar.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+
+      navLinks.forEach(nav => nav.classList.remove("active"));
+
+      link.classList.add("active");
+    });
+  });
+});
